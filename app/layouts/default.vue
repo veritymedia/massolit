35320cdf1306c2eaf8d335cd7a643becbb4ed0a7
Yegor Zakharov <yegor.zakharov99@gmail.com>
2025-04-26 01:06:36 +0200

    move to default layout to not repeat

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,16 +1,28 @@
 <script lang="ts" setup>
-const appConfig = useAppConfig()
-const pb = usePocketbase()
+const appConfig = useAppConfig();
+const pb = usePocketbase();
 onMounted(() => {
-  const url = window.location.host
-  if (!url.includes('localhost')) {
-    pb.baseUrl = "https://" + url
+  const url = window.location.host;
+  if (!url.includes("localhost")) {
+    pb.baseUrl = "https://" + url;
   }
-})
+});
 </script>
 
 <template>
-  <div class="layout-empty">
+  <div
+    class="flex flex-col justify-start w-screen h-screen p-5 background-gradient"
+  >
+    <div class="flex items-center justify-between">
+      <NuxtLink to="/">
+        <img
+          src="../public/images/logos/massolit-logo.png"
+          class="w-24"
+          alt=""
+        />
+      </NuxtLink>
+      <p class="text-sm">{{ pb.authStore.model?.email }}</p>
+    </div>
     <slot />
   </div>
 </template>
